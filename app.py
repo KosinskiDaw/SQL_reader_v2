99a6029addf0227e3a70f6691c026fdbd22e554a
Dawid <kosinskidawid96@gmail.com>
2025-02-21 18:07:31 +0100

    21.02.2025 conn param read

--- app.py
+++ app.py
@@ -8,25 +8,28 @@ import lib.read_data_plc as plc
 import lib.txt_operation as txt_operation
 import threading
 import time
-import snap7
+
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='eventlet')  
 """ ------------------------ Parameters --------------------------- """
 conn_param = txt_operation.read_lines_conn_param()
-print(conn_param)
 IP = str(conn_param[0])
 rack = int(conn_param[1])
 slot = int(conn_param[2])
 db_no = int(conn_param[3])
 start_byte = int(conn_param[4])
 end_byte = int(conn_param[5])
+
+db_param_name = txt_operation.read_lines_config(0)
+print(db_param_name)
+db_param_var = txt_operation.read_lines_config(1)
+print(db_param_var)
+db_param_index = txt_operation.read_lines_config(2)
+print(db_param_index)
 """ --------------------------------------------------------------- """
 
 running = True  
-
- 
-
 
 
 conn = generalS7.connect(IP, rack, slot)
@@ -36,13 +39,21 @@ print(f"Connection status: {conn}")
 
 def read_data_from_PLC():
     global conn
-
+    global IP, rack, slot, db_no, start_byte, end_byte
     while running:
         if conn:  
             try:
                 # Read request
                 read_request = plc.read_data_from_PLC(generalS7, db_no, start_byte, end_byte, conv, "get_bool", 14, 0)
                 if read_request is None:
+                    # Read connection parameters
+                    conn_param = txt_operation.read_lines_conn_param()
+                    IP = str(conn_param[0])
+                    rack = int(conn_param[1])
+                    slot = int(conn_param[2])
+                    db_no = int(conn_param[3])
+                    start_byte = int(conn_param[4])
+                    end_byte = int(conn_param[5])
                     raise Exception("PLC not responding.")
                 # Live bit
                 socketio.emit('update_data1', {'data': plc.read_data_from_PLC(generalS7, db_no, start_byte, end_byte, conv, "get_bool", 0,0)})
